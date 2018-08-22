c55a786f5234306f0bb85b26361da8b9bf2b1a40
Anthony <anthony.kilman+github@gmail.com>
2018-08-22 16:14:01 -0700

    Moar var bits

---
+++ src/main/java/VarKeyword.java
@@ -0,0 +1,16 @@
+public class VarKeyword {
+
+    public static void main(String[] args) {
+
+        var x = true ? "gon' give it to ya" : 10;
+        System.out.println();
+        System.out.println(x.getClass());
+
+        // le sad
+//        var y = VarKeyword::main;
+//        var z = () -> {
+//            System.out.println("test");
+//        };
+
+    }
+}
